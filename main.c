c05d94c3dec35db1ef1100ec71ec853371626091
David Horvath <dhorvath@c3r5p5.hive.fi>
2023-12-10 18:37:57 +0200

    put back function implemented

--- main.c
+++ main.c
@@ -6,18 +6,25 @@
 /*   By: dhorvath <dhorvath@hive.student.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 15:12:00 by dhorvath          #+#    #+#             */
-/*   Updated: 2023/12/10 18:27:33 by dhorvath         ###   ########.fr       */
+/*   Updated: 2023/12/10 18:37:26 by dhorvath         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	sort(t_stack *stack);
+
 int	main(void)
 {
-	
+	int f[5] = {3,2,1,4,5};
+	t_stack a;
+
+	a = (t_stack){.s=&f[0], .size=5};
+	sort(&a);
+	print_list(&a);
 }
 
-void	sort(t_stack *stack, int block_size)
+void	sort(t_stack *stack)
 {
 	// "sort" the first blocksize elements, repeat until empty or 1-2 elements
 	//		find median, above top, under bottom
@@ -29,30 +36,38 @@ void	sort(t_stack *stack, int block_size)
 	put_back(stack, &new_stack);
 }
 
+void	put_back(t_stack *a, t_stack *b)
+{
+	while (b->size)
+	{
+		push(b, a);
+	}
+}
+
 void	semi_sort(t_stack *a, t_stack *b)
 {
 	int min_steps;
 	int min_index;
 	int	initial_rot;
 	
-	while (a->size > 2)
+	while (a->size > 0)
 	{
 		/* find minimum step movement */
 		min_steps = 100000;
 		initial_rot = 0;
 		while (initial_rot < a->size)
 		{
-			if (get_b_rot(a->s[initial_rot], b) + min(initial_rot, a->size - initial_rot) < min_steps)
+			if (abs(get_b_rot(a->s[initial_rot], b)) + min(initial_rot, a->size - initial_rot) < min_steps)
 			{
 				min_index = initial_rot;
-				min_steps = get_b_rot(a->s[initial_rot], b) + min(initial_rot, a->size - initial_rot);
+				min_steps = abs(get_b_rot(a->s[initial_rot], b)) + min(initial_rot, a->size - initial_rot);
 			}
 			initial_rot++;
 		}
 		/* do min step move NEEDS OPTIMSING EASY STEPS */
+		int b_rot = get_b_rot(a->s[min_index], b);
 		while (min_index-- > 0)
 			rotate_list(a);
-		int b_rot = get_b_rot(a->s[initial_rot], b);
 		if (b_rot > 0)
 		{
 			while (b_rot-- > 0)
@@ -66,8 +81,6 @@ void	semi_sort(t_stack *a, t_stack *b)
 		}
 		push(a, b);
 	}
-	if (a->s[0] > a->s[1])
-		switch_top(a);
 }
 
 int	get_b_rot(int val, t_stack *b)
@@ -77,7 +90,7 @@ int	get_b_rot(int val, t_stack *b)
 	if (val < b->s[0] && val < b->s[b->size - 1])
 		return (0);
 	rot = 1;
-	while (rot < b->size - 2)
+	while (rot < b->size - 1)
 	{
 		if (val < b->s[rot - 1] && val > b->s[rot + 1])
 		{
@@ -85,6 +98,7 @@ int	get_b_rot(int val, t_stack *b)
 				return (rot);
 			return (-1 * (b->size - rot));
 		}
+		rot++;
 	}
 	return (-1);
 }
